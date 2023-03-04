8f6e6318193b7154a2591924b1bc9467dca6c88c
Shakil53 <shakilahmed.dcc@gmail.com>
2023-03-05 01:15:15 +0600

    course style done

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { useLoaderData } from 'react-router';
 import Course from '../components/Header/Course/Course';
+import './Home.css';
 
 const Home = () => {
     const courses = useLoaderData();
 
 
     return (
-        <div className='container '>
+        <div className='container'>
             {
                 courses.map(course => <Course course={course}></Course>)
             }
--- src/components/Header/Course/Course.js
+++ src/components/Header/Course/Course.js
@@ -1,13 +1,23 @@
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCode } from '@fortawesome/free-solid-svg-icons'
 import React from 'react';
 import './Course.css';
 
 const Course = ({ course }) => {
-    console.log(course);
+
     return (
-        <div className='courses'>
-            <img alt='courses' src={course.picture}></img>
-            <button>Start Course </button>
-        </div>
+
+        <div className='card course shadow' >
+            <img alt="img" src={course.picture} />
+            <button className='mt-2 btn btn-secondary btn-sl fs-6 fw-semibold'>Start Course
+                <FontAwesomeIcon className='ms-3' icon={faCode}></FontAwesomeIcon>
+            </button>
+
+        </div >
+
+
+
     );
 };
 
